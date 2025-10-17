2cb30dca54d95382c838f8d92371cda17f81e276
tuhin <tuhin.thakur1233@gmail.com>
2025-10-17 15:36:18 +0530

    Refactor: update API_BASE_URL to use the production endpoint for deployment

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8989';
-// export const API_BASE_URL = 'https://drqnz8tvek10e.cloudfront.net';
+// export const API_BASE_URL = 'http://localhost:8989';
+export const API_BASE_URL = "https://hobi.tuhinthakur.me"
